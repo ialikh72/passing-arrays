4a9996cec4233464b492566a1e9f28496602a1f0
ialikh72 <ialikh.w1@gmail.com>
2026-03-02 23:58:51 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Array1(new int[] { 1, 2, 3 });
+        Sum(new int[] { 1, 2, 3 });
+    }
+    static void Array1(int[] arr)
+    {
+        foreach(int a in arr)
+            Console.WriteLine(a);
+    }
+    static void Sum(int[] arr)
+    {
+        int sum = 0;
+        for( int i=0;i<arr.Length;i++)
+             sum += arr[i];
+        Console.WriteLine("sum is:"+sum);
+
+    }
+}
